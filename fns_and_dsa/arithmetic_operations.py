33bc787a8a1c1ec4f331c4feed50eaf3b1afbd20
kyracnh <root@Onmazir.localdomain>
2024-09-26 12:22:08 +0100

    0. Arithmetic Operations Function

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -6,9 +6,9 @@ def perform_operation(num1, num2, operation):
     elif operation == "multiply":
             return num1 * num2
     elif operation == "divide":
-            if num2 != 0:
-                return num1 / num2
-            else:
+            if num2 == 0:
                 print("Sorry, you can not divide by Zero")
+            else:
+                return num1 / num2
     else:
         print("Retry!")
