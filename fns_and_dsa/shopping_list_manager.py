76beaee3aeecc29c99116702d70324e3fc7e850e
kyracnh <root@Onmazir.localdomain>
2024-09-27 16:50:16 +0100

    1. Shopping List Manager

---
+++ fns_and_dsa/shopping_list_manager.py
@@ -0,0 +1,25 @@
+def display_menu():
+    print("Shopping list manager")
+    print("1. Add Item")
+    print("2. Remove Item")
+    print("3. View Item")
+    print("4. Exit")
+def main():
+    shopping_list = []
+    while True:
+        display_menu()
+        choice = input("Enter your choice: ")
+
+        if choice == "1":
+            shopping_list.append(input("Write an Item: "))
+        elif choice == "2":
+            shopping_list.remove(input("Remove an Item: "))
+        elif choice == "3":
+            for item in shopping_list:
+                print(item)
+        elif choice == "4":
+            print("Chrih a3chiri ^^")
+            break
+        else:
+            print("Invalid choice. Please try again.")
+main()
